d6b633cb61a8453783d95f4f8540155dfef8476c
anastminsk <anast-minsk@yandex.ru>
2018-08-18 03:05:23 +0300

    refactor: split assignments template into components

---
+++ src/app/components/Assignments/CardRejected.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { classNames } from 'core/styles';
+import { Card, CardHeader, CardBody, CardTitle, CardText } from 'reactstrap';
+
+const cn = classNames(require('./index.scss'));
+
+type Props = {
+    title: string;
+    description: string;
+};
+
+const CardRejected = (props: Props) => {
+    const { title, description } = props;
+    return (
+        <Card className={cn('card border-danger')}>
+            <CardHeader className={cn('text-white bg-danger')}>The deadline has passed</CardHeader>
+            <CardBody className={cn('card-body')}>
+                <CardTitle className={cn('card-title')}>{title}</CardTitle>
+                <CardText>{description}</CardText>
+            </CardBody>
+        </Card>
+    );
+};
+
+export default CardRejected;
--- src/app/components/Assignments/assignmentsTemplates.tsx
+++ src/app/components/Assignments/assignmentsTemplates.tsx
@@ -1,9 +1,7 @@
 import { IAssignment, IAssignmentStatus } from 'core/models';
 import * as React from 'react';
-import { classNames } from 'core/styles';
-import { Card, CardHeader, CardBody, CardTitle, CardText } from 'reactstrap';
-
-const cn = classNames(require('./index.scss'));
+import CardChecked from './CardChecked';
+import CardRejected from './CardRejected';
 
 export enum FeedActions {
     'Assignment' = 'Assignment',
@@ -12,46 +10,16 @@ export enum FeedActions {
 export const assignmentTemplates: any = {
     [FeedActions.Assignment]: {
         [IAssignmentStatus.Checked]: (feedRecord: IAssignment): any => {
-            if (feedRecord.score >= 100) {
-                return (
-                    <div>
-                        <Card className={cn('card-success border-success')}>
-                            <CardHeader className={cn('bg-success text-white')}>
-                                Done! <span className={cn('score-success')}>{feedRecord.score}%</span>
-                            </CardHeader>
-                            <CardBody className={cn('card-success-body')}>
-                                <CardTitle className={cn('card-success-title')}>{feedRecord.taskId}</CardTitle>
-                                <CardText>{feedRecord.assignmentRepo}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                );
-            } else if (feedRecord.score <= 50) {
-                return (
-                    <Card className={cn('card-warning border-warning')}>
-                        <CardHeader className={cn('bg-warning text-white')}>
-                            Not submitted yet! <span className={cn('score-warning')}>{feedRecord.score}%</span>
-                        </CardHeader>
-                        <CardBody className={cn('card-warning-body')}>
-                            <CardTitle className={cn('card-warning-title')}>{feedRecord.taskId}</CardTitle>
-                            <CardText>{feedRecord.assignmentRepo}</CardText>
-                        </CardBody>
-                    </Card>
-                );
-            }
+            return (
+                <CardChecked
+                    score={feedRecord.score}
+                    title={feedRecord.taskId}
+                    description={feedRecord.assignmentRepo}
+                />
+            );
         },
         [IAssignmentStatus.Rejected]: (feedRecord: IAssignment): any => {
-            return (
-                <div>
-                    <Card className={cn('card-danger border-danger')}>
-                        <CardHeader className={cn('bg-danger text-white')}>The deadline has passed</CardHeader>
-                        <CardBody className={cn('card-danger-body')}>
-                            <CardTitle className={cn('card-danger-title')}>{feedRecord.taskId}</CardTitle>
-                            <CardText>{feedRecord.assignmentRepo}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-            );
+            return <CardRejected title={feedRecord.taskId} description={feedRecord.assignmentRepo} />;
         },
     },
 };
